409df08e7220b6901f77a4b6dd78c63ed3472f44
Arkit Patel <arkitpatel3697@gmail.com>
2018-01-07 01:00:20 +0530

    Mini Project K Means clustering

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -12,7 +12,7 @@ import numpy
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
+from tools.feature_format import featureFormat, targetFeatureSplit
 
 
 
@@ -48,29 +48,60 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
+max_val = 0
+for name in data_dict:
+    if data_dict[name]['exercised_stock_options'] != "NaN":
+        if data_dict[name]['exercised_stock_options'] >= max_val:
+            max_val = data_dict[name]['exercised_stock_options']
+min_val = max_val
+for name in data_dict:
+    if data_dict[name]['exercised_stock_options'] != "NaN":
+        if data_dict[name]['exercised_stock_options'] <= min_val:
+            min_val = data_dict[name]['exercised_stock_options']
+print "max exercised stock options =", max_val
+print "min exercised stock options =", min_val
 
+
+max_val = 0
+for name in data_dict:
+    if data_dict[name]['salary'] != "NaN":
+        if data_dict[name]['salary'] >= max_val:
+            max_val = data_dict[name]['salary']
+min_val = max_val
+for name in data_dict:
+    if data_dict[name]['salary'] != "NaN":
+        if data_dict[name]['salary'] <= min_val:
+            min_val = data_dict[name]['salary']
+print "max salary =", max_val
+print "min salary =", min_val
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+feat1 = list()
+feat2 = list()
+for f1, f2, f3 in finance_features:
+    feat1.append(f1)
+    feat2.append(f3)
+plt.scatter( feat1, feat2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
-
+from sklearn.cluster import KMeans
+cluster = KMeans(n_clusters=2)
+cluster.fit(finance_features)
+pred = cluster.predict(finance_features)
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters2.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
