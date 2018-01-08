5c83bac1645f58ae11efbc706acc0855cf656f82
Arkit Patel <arkitpatel3697@gmail.com>
2018-01-08 20:55:48 +0530

    Mini Project Evaluation

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -14,7 +14,7 @@
 import pickle
 import sys
 sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
+from tools.feature_format import featureFormat, targetFeatureSplit
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -27,5 +27,25 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
-
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+count = 0
+for num in labels_test:
+    if num == 1:
+        count += 1
+print count
+print len(labels_test)
+zero = [0.0]*len(labels_test)
+print accuracy_score(labels_test, zero)
+count = 0
+for i in range(len(labels_test)):
+    if labels_test[i] == 1 and labels_test[i] == pred[i]:
+        count += 1
+print count
+print precision_score(labels_test, pred)
+print recall_score(labels_test, pred)
